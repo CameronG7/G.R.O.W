c49628f641f57ef9500cbdf661864b4c3f8078c7
Liza Smirnov <krasnienkov@gmail.com>
2023-07-15 09:47:35 -0700

    back end boiler plate?

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,20 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    getAllUsers: async () => {
+      return User.find();
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
