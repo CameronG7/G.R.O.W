e47449042dfa08cffca70b3999d9ee15a04abe20
eromeroguti <eromeroguti@gmail.com>
2023-07-17 20:51:26 -0700

    small changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,6 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@ap
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { setContext } from '@apollo/client/link/context';
 
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 
@@ -12,11 +10,11 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
+
   const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
+ 
   return {
     headers: {
       ...headers,
