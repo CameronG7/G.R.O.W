1397b54754eedda33ef46c672b06c1a7bba5f69f
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-07-23 16:46:28 -0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './index.css';
+import App from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
