7e1576b0b5fee8f89cd22135950fdcc1b82dfdf4
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-07-24 18:34:39 -0700

    heroku vite test

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react() ],
+  plugins: [react()],
   server: {
-    port:5173,
+    port: 5173,
     open: true,
     proxy: {
       "/graphql": {
