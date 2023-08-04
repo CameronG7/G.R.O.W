4f8254d919613349d2d986a98dd2d99366ed04ea
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-08-03 19:36:57 -0700

    heroku vite test

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 5174;
+const PORT = process.env.PORT || 5000;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
@@ -15,15 +15,15 @@ const server = new ApolloServer({
 
 
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 
