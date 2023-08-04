90c57354c7590b2cfc4966bab28bf6fcaa3a14d1
CameronG7 <122698132+CameronG7@users.noreply.github.com>
2023-08-03 19:52:59 -0700

    heroku vite test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: '/graphql' ,
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
